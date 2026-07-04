1641b60242c0eefe9c6f09986a2d0bf10b6dd35e
Peter Toennies <toennies@us.ibm.com>
2026-07-04 11:52:57 -0500

    change task background to red

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -9,6 +9,7 @@ type TaskProps = {
   onPinTask: (id: string) => void;
 };
 
+
 export default function Task({
   task: { id, title, state },
   onArchiveTask,
@@ -39,6 +40,7 @@ export default function Task({
           name="title"
           id={`title-${id}`}
           placeholder="Input title"
+         style={{ backgroundColor: 'red' }}
         />
       </label>
       {state !== "TASK_ARCHIVED" && (
